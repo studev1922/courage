73b07a694964340d48423590f9b3052abb891f9f
studev <git@github.com:studev1922>
2023-06-22 17:14:20 +0700

    Jun 22, 2023 5:13 PM first commit

---
+++ src/main/java/courage/courage/CourageApplication.java
@@ -0,0 +1,13 @@
+package courage.courage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourageApplication.class, args);
+	}
+
+}
