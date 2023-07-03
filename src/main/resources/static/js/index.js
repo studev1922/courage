8a860f0b562460b1e98b04587f56cc78a6d26014
studev <git@github.com:studev1922>
2023-07-03 20:09:36 +0700

    embedded js on server

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,28 +1,5 @@
-const { body } = document;
-const view = document.querySelector('[view]');
-const listActions = document.querySelectorAll('[set-view]');
+const app = angular.module('app', []);
 
-listActions.forEach(set =>
-   set.addEventListener('click', e => {
-      let target = e.target, script = document.createElement('script');
-      let sourceView = target.getAttribute('set-view');
-      let sourceJS = target.getAttribute('set-script');
+app.controller('control', ($scope, $http) => {
 
-      rawFile(sourceView, value => {
-         view.innerHTML = value; // set html
-         body.removeAttribute(document.querySelector('script[script-replace]'));
-
-         if (sourceJS) { //set file script if exist
-            script.setAttribute('src', sourceJS);
-            script.setAttribute('script-replace', null);
-            body.appendChild(script);
-         }
-      });
-   })
-);
-
-const rawFile = (source, callback) =>
-   fetch(source)
-      .then(res => res.text())
-      .then(res => callback(res))
-      .catch(err => console.error(err));
+});
