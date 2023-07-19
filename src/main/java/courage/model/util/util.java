e5449f4f01a938278ea33ef15fccd63d2339b407
studev1922 <git@github.com:studev1922>
2023-07-19 08:46:45 -0700

    util

---
+++ src/main/java/courage/model/util/util.java
@@ -0,0 +1,23 @@
+package courage.model.util;
+
+public class util {
+    
+    static String[] merger(Object[]... data) {
+        // get the total length of the merged array
+        int length = 0;
+        for (Object[] array : data)
+            length += array.length;
+
+        // create a new array with the same length
+        String[] merged = new String[length];
+
+        int index = 0; // copy the elements from each array to the merged array
+        // use System.arraycopy() instead of for loop
+        for (Object[] array : data) {
+            System.arraycopy(array, 0, merged, index, array.length);
+            index += array.length;
+        }
+        // return the merged array
+        return merged;
+    }
+}
