95e31d77406a302942a0a84e586d786c78cc812f
studev1922 <git@github.com:studev1922>
2023-07-19 08:44:55 -0700

    SecurityFilterChain

---
+++ src/main/java/courage/AppConfiguration.java
@@ -0,0 +1,30 @@
+package courage;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import courage.model.util.Authorization;
+
+@Configuration
+@EnableWebSecurity
+public class AppConfiguration {
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable().cors().disable();
+        Authorization.authenticate(http);
+        Authorization.securityConfig(http);
+        // http.oauth2ResourceServer().jwt().jwkSetUri(jwk);
+        return http.build();
+    }
+}
--- src/main/java/courage/controller/control/PageControl.java
+++ src/main/java/courage/controller/control/PageControl.java
@@ -1,8 +1,12 @@
 package courage.controller.control;
 
+import java.security.Principal;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping({ "", "/" })
@@ -11,6 +15,12 @@ public class PageControl {
    @GetMapping("/server")
    public String index() {
       return "index";
+   }
+
+   @ResponseBody
+   @GetMapping("/principal")
+   public ResponseEntity<Principal> retrievePrincipal(Principal principal) {
+      return ResponseEntity.ok(principal);
    }
 
    @GetMapping
